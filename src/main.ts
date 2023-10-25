8536859691e1aa8b098a9f79d7ad978ce341ba21
Sebastian Resendiz <sebas.r.2599@gmail.com>
2023-10-25 02:13:37 -0600

    Added Swagger for api documentation

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,17 @@
 import { NestFactory } from '@nestjs/core';
+import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  const config = new DocumentBuilder()
+    .setTitle('ERP Puesto Basañez')
+    .setDescription('API for Puesto Basañez')
+    .setVersion('1.0')
+    // .addTag('add tags here')
+    .build();
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api', app, document);
   await app.listen(3000);
 }
 bootstrap();
