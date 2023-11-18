4ada7d72b557c01dcd39e3ae4ce786d4feb29f40
RivasEmilio <miles.fra.2000@gmail.com>
2023-11-17 23:17:50 -0600

    new migration

---
+++ prisma/migrations/20231118051704_stable1/migration.sql
@@ -0,0 +1,5 @@
+-- AlterTable
+ALTER TABLE "Inventory" ALTER COLUMN "quantity" SET DATA TYPE DOUBLE PRECISION;
+
+-- AlterTable
+ALTER TABLE "OrderDetail" ALTER COLUMN "quantity" SET DATA TYPE DOUBLE PRECISION;
